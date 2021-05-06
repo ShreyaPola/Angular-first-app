23f0abef2f803a0034d4feabd40b19609b05c372
Shreya <pola.saishreya@gmail.com>
2021-05-06 19:02:24 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ /* data:string; 
+  arr = [];
+  // to add an item
+  storeData()
+  {
+    console.log(this.data);
+    this.arr.push(this.data);
+    this.data = '';
+  }
+  /*status:boolean = false;
+  changeStatus()
+  {
+    this.status = !this.status;
+  }
+
+  //To delete an item
+  deleteItem(idx)
+  {
+    this.arr.splice(idx,1)
+  }*/
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Card1Component } from './card1/card1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Card1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
